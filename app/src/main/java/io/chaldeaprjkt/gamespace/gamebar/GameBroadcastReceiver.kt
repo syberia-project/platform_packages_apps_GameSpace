DerpFest-AOSP/packages_apps_GameSpace

GameSpace: add proper permission to the receiver

app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBroadcastReceiver.kt
@@ -54,7 +54,8 @@ class GameBroadcastReceiver : BroadcastReceiver() {
             ?.forEach {
                 (intent.clone() as Intent).apply {
                     setPackage(it)
-                    sendBroadcastAsUser(this, UserHandle.ALL)
+                    sendBroadcastAsUser(this, UserHandle.ALL,
+                        android.Manifest.permission.MANAGE_GAME_MODE)
                 }
             }
     }
@@ -64,4 +65,3 @@ class GameBroadcastReceiver : BroadcastReceiver() {
         const val GAME_STOP = "io.chaldeaprjkt.gamespace.action.GAME_STOP"
     }
 }
-

syberia-project/platform_packages_apps_GameSpace

Signed-off-by: DennySPb <dennyspb@gmail.com>

app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBroadcastReceiver.kt
@@ -55,7 +55,8 @@ class GameBroadcastReceiver : BroadcastReceiver() {
             ?.forEach {
                 (intent.clone() as Intent).apply {
                     setPackage(it)
-                    sendBroadcastAsUser(this, UserHandle.CURRENT)
+                    sendBroadcastAsUser(this, UserHandle.CURRENT,
+                        android.Manifest.permission.MANAGE_GAME_MODE)
                 }
             }
     }
@@ -65,4 +66,3 @@ class GameBroadcastReceiver : BroadcastReceiver() {
         const val GAME_STOP = "io.chaldeaprjkt.gamespace.action.GAME_STOP"
     }
 }
-