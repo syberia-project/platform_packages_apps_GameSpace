392fad29d3e5a1a85f475aa14e4dbe08f0bb452b
Nauval Rizky <enuma.alrizky@gmail.com>
2023-03-02 18:42:11 +0700

    GameSpace: use relative-location on landscape as well
    We already use scrollview anyway, so countless grid will still be
    accessible even with wrapped height.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/widget/PanelView.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/widget/PanelView.kt
@@ -24,12 +24,10 @@ import android.view.LayoutInflater
 import android.widget.TextView
 import android.view.View
 import android.view.WindowInsets
-import android.view.WindowManager
 import android.widget.LinearLayout
 import androidx.core.view.doOnLayout
 import io.chaldeaprjkt.gamespace.R
 import io.chaldeaprjkt.gamespace.utils.dp
-import io.chaldeaprjkt.gamespace.utils.isPortrait
 import kotlin.math.max
 import kotlin.math.min
 
@@ -62,22 +60,14 @@ class PanelView @JvmOverloads constructor(
     }
 
     private fun applyRelativeLocation() {
-        val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        layoutParams.height = LayoutParams.WRAP_CONTENT
-
         doOnLayout {
             if (defaultY == null)
                 defaultY = y
 
-            y = if (wm.isPortrait()) {
-                val safeArea = rootWindowInsets.getInsets(WindowInsets.Type.systemBars())
-                val minY = safeArea.top + 16.dp
-                val maxY = safeArea.top + (parent as View).height - safeArea.bottom - height - 16.dp
-                min(max(relativeY, minY), maxY).toFloat()
-            } else {
-                defaultY ?: 16f
-            }
-
+            val safeArea = rootWindowInsets.getInsets(WindowInsets.Type.systemBars())
+            val minY = safeArea.top + 16.dp
+            val maxY = safeArea.top + (parent as View).height - safeArea.bottom - height - 16.dp
+            y = min(max(relativeY, minY), maxY).toFloat()
         }
     }
 
